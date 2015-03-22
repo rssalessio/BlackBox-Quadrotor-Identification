1550152425462f78baf8b89b777b79be9cdd1982
rssalessio <russo.alessio890@gmail.com>
2015-03-22 17:19:18 +0100

    Changed identify in order to use identifyoptions

--- src/identify.m
+++ src/identify.m
@@ -1,82 +1,69 @@
-function [finalModel] = identify(u, y, modelType, maxOrders, idOpt, outputOption)
-% identification(u,y,modelType,idOpt) identifies a polynomial model based on data
+function [finalModel] = identify(u, y, opt)
+% identification(u,y,opt) identifies a polynomial model based on data
 % (u,y)
 %INPUT:
 %   u: measured input data
 %   y: measured output data
-%   modelType : type of model to be identified, choices =
-%   'arx','armax','oe','bj'
-%   maxOrders : na, nb, nc, nd
-%   idOpt: identification option, can be either 'simulation' or
-%   'prediction'
+%   opt: object of type identifyOptions, used to set options.
 %OUTPUT:
 %   finalModel: model identified
 
     data = iddata(y,u);
-    nk = delayest(data); %estimate time delay (dead time) - also a step response could be used(it's the difference between the first u!= 0 and y!=0)
-
+   
     switch(nargin)
-        case 3
-            maxOrders = [20 20 20 20];
-            idOpt = 'simulation';
-            outputOption = 'nooutput';
-        case 4
-            idOpt = 'simulation';
-            outputOption = 'nooutput';
-        case 5
-            outputOption = 'nooutput';
-    end
-    
-    numOrders = 4;
-    
-    if strcmp(outputOption, 'nooutput')
-        outputOption = 0;
-    else
-        outputOption = 1;
+        case 2
+            opt = identifyOptions;
     end
  
-    if strcmp(modelType,'arx')
-        if length(maxOrders) ~= 2
+    if strcmp(opt.modelType,'arx')
+        if length(opt.maxOrders) ~= 2
             error('arx model needs 2 orders specifications: na, nb');
         end
-        maxOrders = [maxOrders(1:2) 1 1];
+        opt.maxOrders(3:4) = [ 1 1 ];
         idFunc = @arx;
-        opt = arxOptions;
+        idFuncOpt = arxOptions;
         numOrders = 2;
         
-    elseif strcmp(modelType, 'oe')
-        if length(maxOrders) ~= 2
+    elseif strcmp(opt.modelType, 'oe')
+        if length(opt.maxOrders) ~= 2
             error('oe model needs 2 orders specifications: na, nb');
         end
-        maxOrders = [maxOrders(1:2) 1 1];
+        opt.maxOrders(3:4) = [ 1 1 ];
         idFunc = @oe;
-        opt = oeOptions;
+        idFuncOpt = oeOptions;
         numOrders = 2;
         
-    elseif strcmp(modelType, 'armax')
-        if length(maxOrders) ~= 3
+    elseif strcmp(opt.modelType, 'armax')
+        if length(opt.maxOrders) ~= 3
             error('armax model needs 3 orders specifications: na, nb, nc');        
         end
-        
+        opt.maxOrders(4) = 1;
         idFunc = @armax;
-        opt = armaxOptions;
+        idFuncOpt = armaxOptions;
         numOrders = 3;
-    elseif strcmp(modelType, 'bj')
-        if length(maxOrders) ~= 4
+    elseif strcmp(opt.modelType, 'bj')
+        if length(opt.maxOrders) ~= 4
             error('bj model needs 4 orders specifications: nb, nc, nd, nf');
-            return;           
         end
         idFunc = @bj;
-        opt = bjOptions;
+        idFuncOpt = bjOptions;
         numOrders = 4;
     else
-        error([modelType ' not known']);
+        error([opt.modelType ' not known']);
         return;
     end
     
-    opt.Focus = idOpt;
+    idFuncOpt.Focus = opt.target;
     
-    orders = [1 1 1 1];
+    if opt.inputDelayAuto
+        opt.maxOrders(5) = delayest(data); %estimate time delay (dead time) - also a step response could be used(it's the difference between the first u!= 0 and y!=0)
+        minNk = opt.maxOrders(5);
+        maxNk = minNk;
+    else
+        minNk = 1;
+        maxNk = opt.maxOrders(5);
+    end
+    
     J = inf; %variance of PE (eps)
     finalModel = 0;
     
@@ -84,24 +71,27 @@ function [finalModel] = identify(u, y, modelType, maxOrders, idOpt, outputOption
         for nb=1:maxOrders(2)
             for nc=1:maxOrders(3)
                 for nd=1:maxOrders(4)
-                    orders =[na nb nc nd];
-                    idModel = idFunc(data, [orders(1:numOrders) nk], opt);
+                    for nk = minNk:maxNk
+                        orders =[na nb nc nd];
+                        idModel = idFunc(data, [orders(1:numOrders) nk], opt);
 
-                    ysim = sim(idModel, u);
-                    eps = y - ysim;
-                    Jtemp = var(eps);
-                    
-                    [~,ratio,~] = isWhite(eps,0.1,0.1,'nooutput');
-                    
-                    if (Jtemp < J)
-                        if outputOption
-                            disp(['New model with orders ' num2str(orders) ' - J: ' num2str(Jtemp) ' isWhite ratio: ' num2str(ratio)]);
+                        ysim = sim(idModel, u);
+                        eps = y - ysim;
+                        Jtemp = var(eps);
+
+                        [~,ratio,~] = isWhite(eps,0.1,0.1,'nooutput');
+
+                        if (Jtemp < J)
+                            if opt.output
+                                disp(['New model with orders ' num2str(orders) ' - J: ' num2str(Jtemp) ' isWhite ratio: ' num2str(ratio)]);
+                            end
+
+                            finalModel = idModel;
+                            J = Jtemp;
                         end
-                        
-                        finalModel = idModel;
-                        J = Jtemp;
                     end
                 end
+            end
         end
     end
 end
--- src/identifyOptions.m
+++ src/identifyOptions.m
@@ -4,10 +4,10 @@ classdef identifyOptions
 
     properties
         maxOrders = [5 5 5 5 2]; % na nb nc nd, nk fixed input delay
-        inputDelay = 'auto'; % choose auto to let identify() choose the best fixed input delay, otherwise choose 'calc'
+        inputDelayAuto = true; % choose true to let identify() choose the best fixed input delay, otherwise false and calculate it based on lastcomponent of maxorders
 
-        target = 'simulation'; %identification target: either simulation or prediction
-        output = 'true';       %true to print output, otherwise false
+        target = 'simulation';       %identification target: either simulation  or prediction
+        output = true;       %true to print output, otherwise false
 
         cost = 'variance';     %select best model based either on : variance error (variance), aic (aic), fpe (fpe) or fit percentage (fit)
     end
