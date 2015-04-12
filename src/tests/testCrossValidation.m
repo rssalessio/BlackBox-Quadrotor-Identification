38cb7693ba53a3ab0015dcb266a0ca13c331798e
rssalessio <russo.alessio890@gmail.com>
2015-04-12 20:26:05 +0200

    Updated report

--- src/tests/testCrossValidation.m
+++ src/tests/testCrossValidation.m
@@ -4,35 +4,34 @@ close all, clear, clc
 [data,~,~] = loaddata();
 
 IdOptions = identifyOptions();
-    IdOptions.output = 0;
+IdOptions.output = 0;
 SimOptions = simOptions();
 
 models = cell(3,1);
-
+J= zeros(3,3);
 for i=1:3
     disp(['*Training on ' num2str(i) ' exp']);
     
     training = data{1,i};
     [models{i},~] = identify(training, IdOptions);
-    printModel( models{i} );
+    %printModel( models{i} );
     
     for j=1:3       
-        if i ~= j                            
-            validation = data{1,j};           
-            J = validate(models{i}, validation, IdOptions.cost, SimOptions);           
-            disp(['Validation on ' num2str(j) ' exp: ' num2str(J)]); disp(' ');
-        end
+       % if i ~= j                            
+            validation = data{3,j};           
+            J(i,j) = validate(models{i}, validation, IdOptions.cost, SimOptions);           
+           % disp(['Validation on ' num2str(j) ' exp: ' num2str(J(i,j))]); disp(' ');
+      %  end
     end
 end
 %%
 figure; hold on; grid;
-plot( roots(models{1}.a), 'O' ); 
-plot( roots(models{2}.a), 'X' );
-plot( roots(models{3}.a), 's' );
-legend('model1', 'model2', 'model3','Location','northwest');
+h = iopzplot(models{1},'b',models{2},'r',models{3},'c');
+showConfidence(h,3);
+legend('model1', 'model2', 'model3');
 
-%model1 has an order more than the other two and 
-disp(' ');
-norm2 = norm(models{2},2);
-norm3 = norm(models{3},2);
-disp(['Distance between model2 and model3: ' num2str(abs(norm2-norm3)) ]);
+% %model1 has an order more than the other two and 
+% disp(' ');
+% norm2 = norm(models{2},2);
+% norm3 = norm(models{3},2);
+% disp(['Distance between model2 and model3: ' num2str(abs(norm2-norm3)) ]);
